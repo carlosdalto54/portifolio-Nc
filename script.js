841874d55fed976f7eb10d7736cac7bed738c944
Carlos Cassiano Dalto <carlos.dalto@escola.pr.gov.br>
2025-11-18 21:09:03 -0300

    condicional

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let texto = document.querySelector("#texto");
-texto.textContext = "Carlos";
 
 let nome = prompt("Qual o seu nome?");
+if(nome == null){
+    texto.textContent = "Seja bem-vindo(a)!";
+}
+else{
+    texto.textContent = nome;
+}
